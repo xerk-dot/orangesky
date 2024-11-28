20d4540cfa519297e717498d944f5c5ff58dfdab
Your Name <whatGravity@protonmail.com>
2024-11-28 09:24:26 -0600

    footer

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="p-6 pt-3 pb-6 flex text-xs text-center mt-3 dark:text-gray-400 text-gray-500 font-mono">
+    <footer className="fixed bottom-0 left-0 right-0 p-6 pt-3 pb-6 flex text-xs text-center dark:text-gray-400 text-gray-500 font-mono z-1000">
       <div className="grow text-left">
         xerk-dot (
         <Link href="https://bsky.app/profile/orangedev.bsky.social" target="_blank">
